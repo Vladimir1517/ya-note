7209ee6ee81763c7744a8c58cbecb7e1c62cc6d6
Vladimir Tyutin <Vladimir30061517@gmail.com>
2026-02-18 15:28:56 +0300

    1

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -1,8 +1,8 @@
 from http import HTTPStatus
 
-from django.urls import reverse
-
 import pytest
+from django.urls import reverse
+from pytest_django.asserts import assertRedirects
 
 
 @pytest.mark.parametrize(
@@ -53,8 +53,28 @@ def test_pages_availability_for_author(author_client, name, note):
     ('notes:detail', 'notes:edit', 'notes:delete'),
 )
 def test_pages_availability_for_different_users(
-        parametrized_client, name, note, expected_status
+        parametrized_client, name, slug_for_args, expected_status
 ):
-    url = reverse(name, args=(note.slug,))
+    url = reverse(name, args=slug_for_args)
     response = parametrized_client.get(url)
     assert response.status_code == expected_status
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('notes:detail', pytest.lazy_fixture('slug_for_args')),
+        ('notes:edit', pytest.lazy_fixture('slug_for_args')),
+        ('notes:delete', pytest.lazy_fixture('slug_for_args')),
+        ('notes:add', None),
+        ('notes:success', None),
+        ('notes:list', None),
+    ),
+)
+# Передаём в тест анонимный клиент, name проверяемых страниц и args:
+def test_redirects(client, name, args):
+    login_url = reverse('users:login')
+    # Теперь не надо писать никаких if и можно обойтись одним выражением.
+    url = reverse(name, args=args)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
